b81342419b0489767803795e6c10539e7147261f
Wes Moberly <wesm87@gmail.com>
2018-03-18 20:44:57 -0400

    Update code style, combine into one file, update dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 // @flow
 
-import styledTransformProxy from 'styled-transform-proxy';
-import { curry } from 'ramda';
+import styledTransformProxy from 'styled-transform-proxy'
+import { curry, compose, update } from 'ramda'
 
-import scopifyTemplate from './scopify-template';
+type ProxyFn = (Array<string>, ...*) => Array<*>;
 
-const styledScopify = curry((selector: string, styled: *) =>
-  styledTransformProxy(scopifyTemplate(selector), styled)
-);
+const scopifyStrings = (selector: string, strings: Array<string>): Array<string> => {
+  const rules = [...strings]
+  const rulesPrefix = `\n${selector} & {`
+  const rulesSuffix = `}\n`
 
-export default styledScopify;
+  const firstRuleIndex = 0
+  const firstRule = rules[firstRuleIndex]
+
+  if (strings.length === 1) {
+    if (firstRule === '') {
+      return rules
+    }
+
+    return [`${rulesPrefix}${firstRule}${rulesSuffix}`]
+  }
+
+  const lastRuleIndex = rules.length - 1
+  const lastRule = rules[lastRuleIndex]
+
+  return compose(
+    update(lastRuleIndex, `${lastRule}${rulesSuffix}`),
+    update(firstRuleIndex, `${rulesPrefix}${firstRule}`),
+  )(rules)
+}
+
+const scopifyTemplate = (scopeSelector: string): ProxyFn => {
+  const scopifiedTemplateFn = (strings, ...interpolations) => [
+    scopifyStrings(scopeSelector, strings),
+    ...interpolations,
+  ]
+
+  return scopifiedTemplateFn
+}
+
+const styledScopify = curry(
+  (selector: string, styled: *) =>
+    styledTransformProxy(scopifyTemplate(selector), styled)
+)
+
+export default styledScopify
